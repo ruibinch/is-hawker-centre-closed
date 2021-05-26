643029030bb205c1317e8c3e03d8a50fe57214a2
Rui Bin Choo <ruibinch@gmail.com>
2021-05-27 00:37:52 +0800

    chore: shift setEnvVars to test helpers folder

--- jest.config.js
+++ jest.config.js
@@ -2,7 +2,7 @@ module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
   roots: ['<rootDir>'],
-  setupFiles: ['<rootDir>/.jest/setEnvVars.js'],
+  setupFiles: ['<rootDir>/test/helpers/setEnvVars.js'],
   coveragePathIgnorePatterns: ['/node_modules/'],
   collectCoverageFrom: [
     '<rootDir>/src/bot/*.ts',
