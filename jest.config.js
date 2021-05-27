706e70bab01717d40aa96ee996eedee5e4505004
Rui Bin Choo <ruibinch@gmail.com>
2021-05-28 03:09:57 +0800

    chore: set coverage threshold to 90%

--- jest.config.js
+++ jest.config.js
@@ -10,5 +10,13 @@ module.exports = {
     '<rootDir>/src/services/**/*.ts',
   ],
   collectCoverage: true,
+  coverageThreshold: {
+    global: {
+      branches: 90,
+      functions: 90,
+      lines: 90,
+      statements: 90,
+    },
+  },
   coverageReporters: ['text', 'text-summary'],
 };
