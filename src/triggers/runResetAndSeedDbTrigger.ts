1564ef84f71abdf46147921f1c9dde3d444c20e0
Rui Bin Choo <ruibinch@gmail.com>
2021-07-14 01:28:02 +0800

    feat: add runResetAndSeedDbTrigger to run every Sun at 4:10am SGT

---
+++ src/triggers/runResetAndSeedDbTrigger.ts
@@ -0,0 +1,18 @@
+import { APIGatewayProxyHandler, APIGatewayProxyResult } from 'aws-lambda';
+
+import { makeCallbackWrapper } from '../aws/lambda';
+import { run as executeManageDb } from '../scripts/manageDb';
+import { run as executeSeedDb } from '../scripts/seedDb';
+
+export const handler: APIGatewayProxyHandler = async (
+  _event,
+  _context,
+  callback,
+): Promise<APIGatewayProxyResult> => {
+  const callbackWrapper = makeCallbackWrapper(callback);
+
+  await executeManageDb('reset');
+  await executeSeedDb({ shouldWriteFile: false });
+
+  return callbackWrapper(204);
+};
