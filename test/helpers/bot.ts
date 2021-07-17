8c89a66be6d3ab8f803110fe083fa0ca17119c39
Rui Bin Choo <ruibinch@gmail.com>
2021-07-17 08:36:43 +0800

    chore: fix formatting

--- test/helpers/bot.ts
+++ test/helpers/bot.ts
@@ -4,29 +4,39 @@ import { bot, notifications } from '../../src/bot/handler';
 import { TelegramMessage } from '../../src/utils/telegram';
 import { makeTelegramMessage } from '../__mocks__/telegram';
 
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-export const makeBotWrapper = (mockCallback: jest.Mock<any, any>) => async (
-  inputText?: string,
-  otherMessageParams: Partial<TelegramMessage> = {},
-): Promise<void> => {
-  const event: APIGatewayProxyEvent = ({
-    body: JSON.stringify(
-      makeTelegramMessage({ text: inputText, ...otherMessageParams }),
-    ),
-    queryStringParameters: {
-      token: 'pokemongottacatchthemall',
-    },
-  } as unknown) as APIGatewayProxyEvent;
+export const makeBotWrapper =
+  (
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    mockCallback: jest.Mock<any, any>,
+  ) =>
+  async (
+    inputText?: string,
+    otherMessageParams: Partial<TelegramMessage> = {},
+  ): Promise<void> => {
+    const event: APIGatewayProxyEvent = {
+      body: JSON.stringify(
+        makeTelegramMessage({ text: inputText, ...otherMessageParams }),
+      ),
+      queryStringParameters: {
+        token: 'pokemongottacatchthemall',
+      },
+    } as unknown as APIGatewayProxyEvent;
 
-  await bot(event, {} as Context, mockCallback);
-};
+    await bot(event, {} as Context, mockCallback);
+  };
 
-export const makeNotificationsWrapper = (
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  mockCallback: jest.Mock<any, any>,
-) => async (): Promise<void> => {
-  await notifications({} as APIGatewayProxyEvent, {} as Context, mockCallback);
-};
+export const makeNotificationsWrapper =
+  (
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    mockCallback: jest.Mock<any, any>,
+  ) =>
+  async (): Promise<void> => {
+    await notifications(
+      {} as APIGatewayProxyEvent,
+      {} as Context,
+      mockCallback,
+    );
+  };
 
 export const assertBotResponse = (
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
