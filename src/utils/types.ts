2f98751fdd001b17427782ee9e58fda1b45d5dd4
Rui Bin Choo <ruibinch@gmail.com>
2021-07-17 12:10:41 +0800

    feat: improve manageDb script output to show errors

--- src/utils/types.ts
+++ src/utils/types.ts
@@ -28,3 +28,13 @@ export function notEmpty<T>(value: T | null | undefined): value is T {
   const _: T = value;
   return true;
 }
+
+export type WrappedPromise<T> = { result: T } | { error: Error };
+
+export async function wrapPromise<T>(
+  promise: Promise<T>,
+): Promise<WrappedPromise<T>> {
+  return promise
+    .then((result) => ({ result }))
+    .catch((err) => ({ error: err }));
+}
