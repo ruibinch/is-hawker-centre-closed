651eb36452bd6d2fc84971c5ce194267b207a0d6
Rui Bin Choo <ruibinch@gmail.com>
2021-07-18 11:52:18 +0800

    feat: add sendDiscordClosuresAdminMessage function

--- src/ext/discord/index.ts
+++ src/ext/discord/index.ts
@@ -3,12 +3,25 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 const client = new Discord.Client();
-const channelId = process.env.DISCORD_ADMIN_CHANNEL_ID ?? '';
+const adminChannelId = process.env.DISCORD_ADMIN_CHANNEL_ID ?? '';
+const closuresAdminChannelId =
+  process.env.DISCORD_CLOSURES_ADMIN_CHANNEL_ID ?? '';
 
 export async function sendDiscordMessage(message: string): Promise<void> {
   await executeBotLogin();
 
-  const channel = await client.channels.fetch(channelId);
+  const channel = await client.channels.fetch(adminChannelId);
+  if (channel instanceof Discord.TextChannel) {
+    await channel.send(message);
+  }
+}
+
+export async function sendDiscordClosuresAdminMessage(
+  message: string,
+): Promise<void> {
+  await executeBotLogin();
+
+  const channel = await client.channels.fetch(closuresAdminChannelId);
   if (channel instanceof Discord.TextChannel) {
     await channel.send(message);
   }
