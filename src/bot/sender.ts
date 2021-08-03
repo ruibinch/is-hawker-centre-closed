a81ae2929b4bb8da211b7faf3f2499750170218d
Rui Bin Choo <ruibinch@gmail.com>
2021-08-04 00:02:17 +0800

    feat: split message if message length exceeds allowed max length of 4096

--- src/bot/sender.ts
+++ src/bot/sender.ts
@@ -1,7 +1,11 @@
 import axios from 'axios';
 
 import { TelegramMessageError } from '../errors/TelegramMessageError';
-import { makeTelegramApiBase, TelegramResponseBase } from '../utils/telegram';
+import {
+  makeTelegramApiBase,
+  TelegramResponseBase,
+  TELEGRAM_MESSAGE_MAX_LENGTH,
+} from '../utils/telegram';
 
 const BOT_TOKEN = process.env.BOT_TOKEN || '';
 
@@ -11,18 +15,59 @@ export async function sendMessage(props: {
 }): Promise<void> {
   const { chatId, message } = props;
 
-  const response: TelegramResponseBase = await axios
-    .get(`${makeTelegramApiBase(BOT_TOKEN)}/sendMessage`, {
-      params: {
-        chat_id: chatId,
-        text: message,
-        parse_mode: 'MarkdownV2',
-        reply_markup: {
-          remove_keyboard: true,
-        },
+  const telegramSendMessageUrl = `${makeTelegramApiBase(
+    BOT_TOKEN,
+  )}/sendMessage`;
+
+  const makeSendMessageParams = (text: string) => ({
+    params: {
+      chat_id: chatId,
+      text,
+      parse_mode: 'MarkdownV2',
+      reply_markup: {
+        remove_keyboard: true,
       },
-    })
-    .then((res) => res.data)
+    },
+  });
+
+  const telegramMessages =
+    message.length > TELEGRAM_MESSAGE_MAX_LENGTH
+      ? (() => {
+          const messageSplit = message.split('\n\n');
+
+          let firstMessageLength = 0;
+          let splitIndex = 0;
+          for (let i = 0; i < messageSplit.length; i += 1) {
+            // 2 represents the length of \n\n delimiter
+            const entryLength = messageSplit[i].length + 2;
+
+            if (
+              firstMessageLength + entryLength >
+              TELEGRAM_MESSAGE_MAX_LENGTH
+            ) {
+              break;
+            }
+
+            firstMessageLength += entryLength;
+            splitIndex = i;
+          }
+
+          // Assumption: message.length will never exceed 4096 * 2
+          const firstMessage = messageSplit
+            .slice(0, splitIndex + 1)
+            .join('\n\n');
+          const secondMessage = messageSplit.slice(splitIndex + 1).join('\n\n');
+
+          return [firstMessage, secondMessage];
+        })()
+      : [message];
+
+  const responses: TelegramResponseBase[] = await Promise.all(
+    telegramMessages.map((telegramMessage) =>
+      axios.get(telegramSendMessageUrl, makeSendMessageParams(telegramMessage)),
+    ),
+  )
+    .then((_responses) => _responses.map((res) => res.data))
     .catch((error) => {
       console.error('[bot > sender > sendMessage]', {
         telegramResponse: error.response.data,
@@ -31,7 +76,7 @@ export async function sendMessage(props: {
       return error.response.data;
     });
 
-  if (!response.ok) {
+  if (responses.some((res) => !res.ok)) {
     throw new TelegramMessageError();
   }
 }
