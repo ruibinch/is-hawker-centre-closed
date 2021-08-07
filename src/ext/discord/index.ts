479246e00c41f4c43460e4fa04c95d9ad5866090
Rui Bin Choo <ruibinch@gmail.com>
2021-08-08 01:02:59 +0800

    chore: split closures admin discord channel to closures-admin-dev and closures-admin-prod

--- src/ext/discord/index.ts
+++ src/ext/discord/index.ts
@@ -5,8 +5,10 @@ dotenv.config();
 const client = new Discord.Client();
 const adminDevChannelId = process.env.DISCORD_ADMIN_DEV_CHANNEL_ID ?? '';
 const adminProdChannelId = process.env.DISCORD_ADMIN_PROD_CHANNEL_ID ?? '';
-const closuresAdminChannelId =
-  process.env.DISCORD_CLOSURES_ADMIN_CHANNEL_ID ?? '';
+const closuresAdminDevChannelId =
+  process.env.DISCORD_CLOSURES_ADMIN_DEV_CHANNEL_ID ?? '';
+const closuresAdminProdChannelId =
+  process.env.DISCORD_CLOSURES_ADMIN_PROD_CHANNEL_ID ?? '';
 
 export async function sendDiscordAdminMessage(message: string): Promise<void> {
   await executeBotLogin();
@@ -22,7 +24,9 @@ export async function sendDiscordClosuresAdminMessage(
 ): Promise<void> {
   await executeBotLogin();
 
-  const channel = await client.channels.fetch(closuresAdminChannelId);
+  const channel = await client.channels.fetch(
+    getClosuresAdminChannelId(message),
+  );
   if (channel instanceof Discord.TextChannel) {
     await channel.send(message);
   }
@@ -34,4 +38,10 @@ async function executeBotLogin() {
 
 function getAdminChannelId(message: string) {
   return message.startsWith('[prod]') ? adminProdChannelId : adminDevChannelId;
+}
+
+function getClosuresAdminChannelId(message: string) {
+  return message.startsWith('[prod]')
+    ? closuresAdminProdChannelId
+    : closuresAdminDevChannelId;
 }
