65097c1205ecb0438b949ee480b90da279f1b053
Rui Bin Choo <ruibinch@gmail.com>
2021-09-09 22:39:25 +0800

    chore(bot): add printBotDescription.ts

---
+++ bot/src/scripts/printBotDescription.ts
@@ -0,0 +1,12 @@
+/**
+ * Print bot description for feeding into Telegram BotFather.
+ */
+console.log(
+  `An easy way to check when your favourite hawker centre is next closed, and more!\n\n` +
+    `This bot can:\n` +
+    `• find the next closure dates of any hawker centre\n` +
+    `• store your favourite hawker centres in a favourites list for easy reference\n` +
+    `• notify you in the morning when one of your favourite hawker centres is closed\n` +
+    `• reply to you in both English and Mandarin\n\n` +
+    `Type /help to find out more!`,
+);
