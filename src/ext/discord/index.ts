411f216b330d8b400940c77b3324616666c68212
Rui Bin Choo <ruibinch@gmail.com>
2022-01-31 11:20:05 +0800

    fix: add 60s timeout to Discord bot login to prevent indefinite retries when Discord API is down

--- src/ext/discord/index.ts
+++ src/ext/discord/index.ts
@@ -10,6 +10,8 @@ const closuresAdminDevChannelId =
 const closuresAdminProdChannelId =
   process.env.DISCORD_CLOSURES_ADMIN_PROD_CHANNEL_ID ?? '';
 
+const DISCORD_BOT_LOGIN_TIMEOUT_MS = 60000;
+
 export async function sendDiscordAdminMessage(message: string): Promise<void> {
   await executeBotLogin();
 
@@ -32,8 +34,38 @@ export async function sendDiscordClosuresAdminMessage(
   }
 }
 
+// Add timeout of 60s to bot login to prevent indefinite retries when Discord API is down
 async function executeBotLogin() {
-  await client.login(process.env.DISCORD_BOT_TOKEN);
+  let timeoutHandle: NodeJS.Timeout;
+  const timeoutPromise = new Promise((_, reject) => {
+    timeoutHandle = setTimeout(
+      () =>
+        reject(
+          new Error(
+            `Discord bot login timed out after ${
+              DISCORD_BOT_LOGIN_TIMEOUT_MS / 1000
+            } seconds`,
+          ),
+        ),
+      DISCORD_BOT_LOGIN_TIMEOUT_MS,
+    );
+  });
+
+  const result = await Promise.race([
+    client.login(process.env.DISCORD_BOT_TOKEN),
+    timeoutPromise,
+  ])
+    .then((res) => {
+      clearTimeout(timeoutHandle);
+      return res;
+    })
+    .catch((error) => {
+      return error;
+    });
+
+  if (result instanceof Error) {
+    throw result;
+  }
 }
 
 function getAdminChannelId(message: string) {
