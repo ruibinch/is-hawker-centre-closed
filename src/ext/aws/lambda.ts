e62aee67f8de3533a1d2ef0a558f01d0bf0c1b30
Rui Bin Choo <ruibinch@gmail.com>
2022-02-05 14:40:23 +0800

    feat: deprecate makeCallbackWrapper and return APIGatewayProxyResult directly

--- src/ext/aws/lambda.ts
+++ src/ext/aws/lambda.ts
@@ -1,18 +1,9 @@
-import type { APIGatewayProxyResult, Callback } from 'aws-lambda';
+import type { APIGatewayProxyResult } from 'aws-lambda';
 
-type ResponseBody = {
-  statusCode: number;
-  body: string;
-};
-
-export const makeCallbackWrapper =
-  (callback: Callback<APIGatewayProxyResult>) =>
-  (statusCode: number, body?: string): ResponseBody => {
-    const responseBody: ResponseBody = {
-      statusCode,
-      body: body ?? '',
-    };
-
-    callback(null, responseBody);
-    return responseBody;
-  };
+export const makeLambdaResponse = (
+  statusCode: number,
+  body?: string,
+): APIGatewayProxyResult => ({
+  statusCode,
+  body: body ?? '',
+});
