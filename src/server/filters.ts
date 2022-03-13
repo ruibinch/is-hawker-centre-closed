345e1515095475ec3cf9b3e5d5c8b430c659020b
Rui Bin Choo <ruibinch@gmail.com>
2022-03-13 12:54:53 +0800

    feat(server): add server filters

---
+++ src/server/filters.ts
@@ -0,0 +1,36 @@
+import { isAfter, isBefore, parseISO } from 'date-fns';
+
+import { Input } from '../models/Input';
+import { User } from '../models/User';
+import { getDateIgnoringTime } from '../utils/date';
+
+export function filterInputByUserId(input: Input, userId: number | undefined) {
+  return userId === undefined || userId === input.userId;
+}
+
+export function filterInputByDate(
+  input: Input,
+  fromDate: string | undefined,
+  toDate: string | undefined,
+) {
+  const inputCreatedDate = parseISO(getDateIgnoringTime(input.createdAt));
+
+  if (fromDate) {
+    const _fromDate = parseISO(fromDate);
+    if (isBefore(inputCreatedDate, _fromDate)) {
+      return false;
+    }
+  }
+  if (toDate) {
+    const _toDate = parseISO(toDate);
+    if (isAfter(inputCreatedDate, _toDate)) {
+      return false;
+    }
+  }
+
+  return true;
+}
+
+export function filterUserByUserId(user: User, userId: number | undefined) {
+  return userId === undefined || userId === user.userId;
+}
---
+++ test/unit/server/filters.test.ts
@@ -0,0 +1,111 @@
+import { parseISO } from 'date-fns';
+
+import { Input } from '../../../src/models/Input';
+import { User } from '../../../src/models/User';
+import {
+  filterInputByDate,
+  filterInputByUserId,
+  filterUserByUserId,
+} from '../../../src/server/filters';
+
+describe('[unit] server > filters', () => {
+  let dateSpy: jest.SpyInstance;
+
+  beforeAll(() => {
+    dateSpy = jest
+      .spyOn(Date, 'now')
+      .mockImplementation(() => parseISO('2021-06-04T15:26:35').valueOf());
+  });
+
+  afterAll(() => {
+    dateSpy.mockRestore();
+  });
+
+  describe('Input filters', () => {
+    let input: Input;
+
+    beforeAll(() => {
+      input = Input.create({
+        inputId: 'dummyInputId',
+        userId: 1,
+        username: 'ashketchum',
+        text: '/start',
+      });
+    });
+
+    describe('filterInputByUserId', () => {
+      it('returns true when input userId matches', () => {
+        expect(filterInputByUserId(input, 1)).toBeTruthy();
+      });
+
+      it('returns true when input userId is undefined', () => {
+        expect(filterInputByUserId(input, undefined)).toBeTruthy();
+      });
+
+      it('returns false when input userId does not match', () => {
+        expect(filterInputByUserId(input, 2)).toBeFalsy();
+      });
+    });
+
+    describe('filterInputByDate', () => {
+      it('returns true when input falls within fromDate and toDate', () => {
+        expect(
+          filterInputByDate(input, '2021-06-01', '2021-06-30'),
+        ).toBeTruthy();
+      });
+
+      it('returns true when input is after the specified fromDate', () => {
+        expect(filterInputByDate(input, '2021-06-01', undefined)).toBeTruthy();
+      });
+
+      it('returns true when input is before the specified toDate', () => {
+        expect(filterInputByDate(input, undefined, '2021-06-30')).toBeTruthy();
+      });
+
+      it('returns true when fromDate and toDate are undefined', () => {
+        expect(filterInputByDate(input, undefined, undefined)).toBeTruthy();
+      });
+
+      it('returns false when input is before the specified fromDate', () => {
+        expect(
+          filterInputByDate(input, '2021-06-05', '2021-06-30'),
+        ).toBeFalsy();
+      });
+
+      it('returns false when input is after the specified toDate', () => {
+        expect(
+          filterInputByDate(input, '2021-06-01', '2021-06-03'),
+        ).toBeFalsy();
+      });
+    });
+  });
+
+  describe('User filters', () => {
+    let user: User;
+
+    beforeAll(() => {
+      user = User.create({
+        userId: 1,
+        username: 'ashketchum',
+        languageCode: 'en',
+        favourites: [],
+        isInFavouritesMode: false,
+        notifications: false,
+      });
+    });
+
+    describe('filterUserByUserId', () => {
+      it('returns true when input userId matches', () => {
+        expect(filterUserByUserId(user, 1)).toBeTruthy();
+      });
+
+      it('returns true when input userId is undefined', () => {
+        expect(filterUserByUserId(user, undefined)).toBeTruthy();
+      });
+
+      it('returns false when input userId does not match', () => {
+        expect(filterUserByUserId(user, 2)).toBeFalsy();
+      });
+    });
+  });
+});
