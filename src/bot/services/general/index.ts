4dee86b5f185a6aac733e1b87e7de9caeaa2df24
Rui Bin Choo <ruibinch@gmail.com>
2022-03-20 10:15:28 +0800

    chore(bot): remove old updates from being returned in /updates

--- src/bot/services/general/index.ts
+++ src/bot/services/general/index.ts
@@ -29,36 +29,5 @@ function getLatestUpdates(): ChangelogEntry[] {
       date: '2021\\-09\\-18',
       details: ['Added search by "next week" timeframe'],
     },
-    {
-      version: '\\[0\\.12\\.0\\]',
-      date: '2021\\-09\\-14',
-      details: [
-        'Changed returned results to always be sorted in alphabetical order',
-      ],
-    },
-    {
-      version: '\\[0\\.11\\.0\\]',
-      date: '2021\\-08\\-08',
-      details: [
-        'Changed default timeframe from "today" to "next" to return more useful information by default',
-      ],
-    },
-    {
-      version: '\\[0\\.10\\.0\\]',
-      date: '2021\\-08\\-07',
-      details: ['Added indefinite end date option'],
-    },
-    {
-      version: '\\[0\\.9\\.0\\]',
-      date: '2021\\-07\\-28',
-      details: ['Added /updates command to check the latest updates'],
-    },
-    {
-      version: '\\[0\\.8\\.0\\]',
-      date: '2021\\-07\\-28',
-      details: [
-        'Added auto\\-expansion of recognised acronyms, e\\.g\\. "amk" to "ang mo kio", "tpy" to "toa payoh"',
-      ],
-    },
   ];
 }
